fcbe33677f97c13da8d48aa7c195c0c13c02877d
Werick Lima <wericklima2019@gmail.com>
2022-03-23 17:12:35 -0300

    Att

--- Faculdade/Java/POO_lista2/src/exercicio_1/Empresa.java
+++ Faculdade/Java/POO_lista2/src/exercicio_1/Empresa.java
@@ -9,77 +9,77 @@ public class Empresa {
 		team = new Funcionario[10];
 		
 		//2 Horistas
-		String nome = lerString("Informe o nome do Funcionário");
-		String sobrenome = lerString("Informe o sobrenome do Funcionário");
+		String nome = lerString("Informe o nome do Funcionï¿½rio");
+		String sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[0] = new Horistas(nome, sobrenome);
 		System.out.println("Cadastrado como Horista");
 		
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[1] = new Horistas(nome, sobrenome);
 		System.out.println("Cadastrado como Horista");
 		
 		//4 Administradores
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[2] = new Administrador(nome, sobrenome);
 		System.out.println("Cadastrado como Administrador");
 		
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[3] = new Administrador(nome, sobrenome);
 		System.out.println("Cadastrado como Administrador");
 		
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[4] = new Administrador(nome, sobrenome);
 		System.out.println("Cadastrado como Administrador");
 		
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[5] = new Administrador(nome, sobrenome);
 		System.out.println("Cadastrado como Administrador");
 		
 		//3 Comisionados
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[6] = new Comissionado(nome, sobrenome);
 		System.out.println("Cadastrado como Comissionado");
 		
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[7] = new Comissionado(nome, sobrenome);
 		System.out.println("Cadastrado como Comissionado");
 		
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[8] = new Comissionado(nome, sobrenome);
 		System.out.println("Cadastrado como Comissionado");
 		
 		//1 Gerente
-		nome = lerString("Informe o nome do Funcionário");
-		sobrenome = lerString("Informe o sobrenome do Funcionário");
+		nome = lerString("Informe o nome do Funcionï¿½rio");
+		sobrenome = lerString("Informe o sobrenome do Funcionï¿½rio");
 		team[9] = new Gerente(nome, sobrenome);
 		System.out.println("Cadastrado como Gerente");
 		
 		//Cadastrando os dados baseado na instancia do objeto
 		for (int i=0; i<=9; i++) {
 			if(team[i] instanceof Gerente) {
-				double salario = lerDouble("Informe o salário do Gerente: " +team[i].getNome());
+				double salario = lerDouble("Informe o salï¿½rio do Gerente: " +team[i].getNome());
 				((Gerente) team[i]).setSalario(salario);
-				double  bonificação = lerDouble("Informe o percentual de bonificação");
-				((Gerente) team[i]).setBonificacao(bonificação);
+				double  bonificacao = lerDouble("Informe o percentual de bonificaï¿½ï¿½o");
+				((Gerente) team[i]).setBonificacao(bonificacao);
 			}
 			else if (team[i] instanceof Administrador) {
-				double salario = lerDouble("Informe o salário do Administrador: "+team[i].getNome());
+				double salario = lerDouble("Informe o salï¿½rio do Administrador: "+team[i].getNome());
 				((Administrador) team[i]).setSalario(salario);
 			}
 			else if (team[i] instanceof Comissionado) {
-				double salario = lerDouble("Informe o salário do Comissionado: " + team[i].getNome());
+				double salario = lerDouble("Informe o salï¿½rio do Comissionado: " + team[i].getNome());
 				((Comissionado) team[i]).setSalario(salario);
 				int total_de_vendas = lerInteiro("Informe o total de vendas do Comissionado: "+team[i].getNome());
 				((Comissionado) team[i]).setTotal_de_vendas(total_de_vendas);
-				double comissao = lerDouble("Informe o percentual de comissão do Comissionado: "+team[i].getNome());
+				double comissao = lerDouble("Informe o percentual de comissï¿½o do Comissionado: "+team[i].getNome());
 				((Comissionado) team[i]).setPercentual_de_comissao(comissao);
 			}
 			else {
@@ -96,7 +96,7 @@ public class Empresa {
 	}
 	
 	
-	//métodos úteis haha
+	//mï¿½todos ï¿½teis haha
 	public static String lerString(String txt) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(txt);
