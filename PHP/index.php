106a8d90ff4cd4caaef9a30110a52cebff8d44e6
Werick Lima <87555656+WerickL@users.noreply.github.com>
2022-09-05 14:17:32 -0300

    init code of PHP

--- PHP/index.php
+++ PHP/index.php
@@ -1,1 +1,12 @@
-<?php echo "Hello world" ?>
+
+<h1>Essa página está sendo escrita em ambiente php</h1>
+<?php 
+$nome = "werick";
+$nome = $_GET['nome'];
+
+echo "Bem vindo  $nome, esse é um exemplo de código PHP.";
+?>
+<form action="index.php">
+    <input type="text" name="nome">
+    <input type="submit">
+</form>
